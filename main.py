93df58534347f14fccb550d203a57fcd9ff83dd3
qfu4 <qfu4@u.rochester.edu>
2020-12-03 23:11:14 +0800

    account page updated
    mainly updated the account page and the main.py file to enable users to update their email address and user name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from app import db
 from models import User, Job, Task
 
 main = Blueprint('main', __name__)
@@ -17,3 +18,22 @@ def dashboard():
 @login_required
 def account():
 	return render_template('account.html', name=current_user.name)
+
+@main.route('/account', methods=['POST'])
+def account_post():
+	email = request.form.get('email')
+	name = request.form.get('name')
+	
+	if current_user.name != name:
+		current_user.name = name
+		db.session.commit()
+	#if no user is found or the password is wrong
+	if current_user.email != email:
+		user = User.query.filter_by(email=email).first()
+		if user:
+			flash('The email address has been registered! Please use another email!')
+		else:
+			current_user.email = email
+			db.session.commit()
+
+	return redirect(url_for('main.account'))
