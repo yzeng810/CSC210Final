63f9be1bdcb4f854c37015a71802892c6cbf740d
qfu4 <qfu4@u.rochester.edu>
2020-12-08 20:53:43 +0800

    database - create function
    test creating job

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from app import db
 from models import User, Job, Task, Assessment
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -40,3 +41,29 @@ def account_post():
 			flash('You email address has been updated!')
 
 	return redirect(url_for('main.account'))
+
+@main.route('/job/new', methods=['GET','POST'])
+@login_required
+def new_job():
+	if request.method == "POST":
+		program = request.form.get('program')
+		company = request.form.get('company')
+		deadline = datetime.strptime(request.form.get('deadline'), "%Y-%m-%d").date()
+		resumeNotes = request.form.get('resumeNotes')
+		coverletterNotes = request.form.get('coverletterNotes')
+		transcriptNotes = request.form.get('transcriptNotes')
+		onlineFormNotes = request.form.get('onlineFormNotes')
+		job = Job(program=program, company=company, deadline=deadline, resume=False, resumeNotes=resumeNotes, coverletter=False, coverletterNotes=coverletterNotes, 
+			transcript=False, transcriptNotes=transcriptNotes, onlineForm=False, onlineFormNotes=onlineFormNotes, user_id=current_user.id)
+		db.session.add(job)
+		db.session.commit()
+		return redirect(url_for('main.job'))
+	return render_template('create_job.html', title='New Application')
+
+
+
+@main.route('/job')
+@login_required
+def job():
+	jobs = Job.query.all()
+	return render_template('job.html', jobs=jobs)
